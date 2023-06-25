e8cd3e1e6ba5cb9c33c679766bda792afdbd7c06
Otsile Thamage <ooats77@gmail.com>
2023-06-25 17:42:35 +0200

    app index

--- src/app.ts
+++ src/app.ts
@@ -26,13 +26,240 @@ app.use(express.json());
 
 const options = {
   definition: {
-    openapi: "3.0.1",
-    info: {
-      title: "REST API for Swagger Documentationz",
-      version: "1.0.0",
+    "openapi": "3.0.3",
+    "info": {
+      "title": "Book Management System",
+      "contact": {
+        "email": "rthamage67@gmail.com"
+      },
+      "license": {
+        "name": "Apache 2.0",
+        "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+      },
+      "version": "1.0.11"
     },
-    schemes: ["http", "https"],
-    servers: [{ url: "http://localhost:3000/" }],
+    "servers": [
+      {
+        "url": "http://localhost:5000"
+      }
+    ],
+    "tags": [
+      {
+        "name": "book",
+        "description": "Access to crud methods"
+      }
+    ],
+    "paths": {
+      "/": {
+        "get": {
+          "tags": [
+            "book"
+          ],
+          "summary": "test endpoint",
+          "responses": {
+            "200": {
+              "description": "Successful operation"
+            }
+          }
+        }
+      },
+      "/book/findBookbyIsbn/{Isbn}": {
+        "get": {
+          "tags": [
+            "book"
+          ],
+          "summary": "Get book by Isbn",
+          "description": "Get Book by ISBN",
+          "operationId": "findBookByIsbn",
+          "parameters": [
+            {
+              "name": "Isbn",
+              "in": "path",
+              "description": "ISBN of book to return",
+              "required": true,
+              "explode": true,
+              "schema": {
+                "type": "string"
+              }
+            }
+          ],
+          "responses": {
+            "200": {
+              "description": "successful operation",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "$ref": "#/components/schemas/Book"
+                  }
+                }
+              }
+            },
+            "403": {
+              "description": "Invalid input"
+            },
+            "404": {
+              "description": "Book not found"
+            }
+          }
+        }
+      },
+      "/book/create": {
+        "post": {
+          "tags": [
+            "book"
+          ],
+          "summary": "Create book",
+          "description": "Create a book",
+          "operationId": "createBook",
+          "requestBody": {
+            "description": "create book",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/Book"
+                }
+              }
+            },
+            "required": true
+          },
+          "responses": {
+            "201": {
+              "description": "Created"
+            },
+            "401": {
+              "description": "Book Already exists"
+            },
+            "403": {
+              "description": "Invalid input"
+            },
+            "405": {
+              "description": "Validation exception"
+            }
+          }
+        }
+      },
+      "/book/update": {
+        "put": {
+          "tags": [
+            "book"
+          ],
+          "summary": "Update an existing book",
+          "description": "Update an existing book by ISBN",
+          "operationId": "updateBook",
+          "requestBody": {
+            "description": "Update an book in the library",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/Book"
+                }
+              }
+            },
+            "required": true
+          },
+          "responses": {
+            "200": {
+              "description": "Successful operation",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "$ref": "#/components/schemas/Book"
+                  }
+                }
+              }
+            },
+            "400": {
+              "description": "Invalid ID supplied"
+            },
+            "404": {
+              "description": "Book not found"
+            },
+            "405": {
+              "description": "Validation exception"
+            }
+          }
+        }
+      },
+      "/book/delete/{Isbn}": {
+        "delete": {
+          "tags": [
+            "book"
+          ],
+          "summary": "Delete a book",
+          "description": "Delete an existing book by ISBN",
+          "operationId": "deleteBook",
+          "parameters": [
+            {
+              "name": "Isbn",
+              "in": "path",
+              "description": "ISBN of book to delete",
+              "required": true,
+              "explode": true,
+              "schema": {
+                "type": "string"
+              }
+            }
+          ],
+          "responses": {
+            "200": {
+              "description": "Successful operation",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "$ref": "#/components/schemas/Book"
+                  }
+                }
+              }
+            },
+            "400": {
+              "description": "Invalid ID supplied"
+            },
+            "404": {
+              "description": "Book not found"
+            },
+            "405": {
+              "description": "Validation exception"
+            }
+          }
+        }
+      }
+    },
+    "components": {
+      "schemas": {
+        "Book": {
+          "type": "object",
+          "properties": {
+            "isbn": {
+              "type": "string",
+              "example": "978-3-16-148410-0"
+            },
+            "bookTitle": {
+              "type": "string",
+              "example": "Harry Potter"
+            },
+            "author": {
+              "type": "string",
+              "example": "John Doe"
+            }
+          },
+          "xml": {
+            "name": "book"
+          }
+        }
+      },
+      "requestBodies": {
+        "Book": {
+          "description": "Book object that is created",
+          "content": {
+            "application/json": {
+              "schema": {
+                "$ref": "#/components/schemas/Book"
+              }
+            }
+          }
+        }
+      }
+    }
   },
   apis: [
     `${__dirname}/routes/example-route.ts`,
