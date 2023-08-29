7cf0cd2a381378e523d5c56ebfedbd102768b0b0
Rethabile Otsile Thamage <ooats77@gmail.com>
2023-08-29 11:53:30 +0200

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/src/setupFilesAfterEnv.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/src/setupFilesAfterEnv.ts"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
